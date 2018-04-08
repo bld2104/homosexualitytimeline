c4ec090c9921a3816aa17bc48aa6370a487a6933
Barb Donnini <bld2104@gmail.com>
2018-04-08 15:12:52 -0400

    cool scrolling feature to timeline

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # History of Christian Views towards Homosexuality
 
 This is for my Foundation in Ethics course as part of my master's degree in Theology from Villanova University.
+
+See the working app here: https://homosexualitytimeline.herokuapp.com/
--- homosexualitytimeline/settings.py
+++ homosexualitytimeline/settings.py
@@ -27,9 +27,10 @@ SECRET_KEY = "Pw>_hm$Rg$1LUP7@H__nwH93YteTn"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 # Application definition
 
