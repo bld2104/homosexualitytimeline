14618fe3630b6cb3a91c5b6c2ed2a9b6003be093
Barb Donnini <bld2104@gmail.com>
2018-04-08 14:08:31 -0400

    trying database again

--- homosexualitytimeline/settings.py
+++ homosexualitytimeline/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-
+import psycopg2
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,12 @@ SECRET_KEY = "Pw>_hm$Rg$1LUP7@H__nwH93YteTn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Application definition
 
 INSTALLED_APPS = [
@@ -107,18 +113,6 @@ WSGI_APPLICATION = 'homosexualitytimeline.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'homosexualitytimeline',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
