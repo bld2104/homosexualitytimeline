c546db7a7e8087a5ef8775aca1b74d7d454de20b
Jeremy Low <jeremylow@gmail.com>
2018-04-13 16:22:11 -0400

    next

--- homosexualitytimeline/wsgi.py
+++ homosexualitytimeline/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homosexualitytimeline.local_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homosexualitytimeline.settings")
 
 application = get_wsgi_application()
